38eca463cde335616351f147deacc1c4237875b9
Razvan Stanciu <raelag53@gmail.com>
2025-11-03 16:50:22 +0200

    Instructiuni introductive SQL

---
+++ Curs1/Intro_SQL.sql
@@ -0,0 +1,57 @@
+
+SHOW DATABASES;
+-- CREATE DATABASE prima_baza_de_date;
+# SHOW DATABASES;
+
+CREATE DATABASE a_doua_baza_de_date;
+
+SHOW DATABASES;
+
+DROP DATABASE a_doua_baza_de_date;
+
+
+CREATE DATABASE IF NOT EXISTS a_treia_baza_de_date;
+DROP DATABASE IF EXISTS a_treia_baza_de_date;
+
+
+SHOW DATABASES;
+
+USE prima_baza_de_date;
+CREATE TABLE calorii (nr_crt INT, calorii INT);
+
+DROP TABLE calorii;
+
+CREATE TABLE IF NOT EXISTS calorii (nr_crt INT, calorii INT);
+
+INSERT INTO calorii VALUES (1, 2500);
+
+SELECT * FROM calorii;
+
+INSERT INTO calorii VALUES (2, 3000);
+
+SELECT * FROM calorii;
+
+INSERT INTO calorii VALUES (3500, 3);
+SELECT * FROM calorii;
+
+
+INSERT INTO calorii SET calorii=2223;
+SELECT * FROM calorii;
+
+DROP TABLE calorii;
+
+
+INSERT INTO calorii SET calorii=2223;
+SELECT * FROM calorii;
+
+
+
+
+
+
+
+
+
+
+
+
