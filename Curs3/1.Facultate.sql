f221cc95ce2e1a145534ea50b0a0a1218e46c764
Razvan Stanciu <raelag53@gmail.com>
2025-11-05 17:56:48 +0200

    Adaugare Curs3

---
+++ Curs3/1.Facultate.sql
@@ -0,0 +1,41 @@
+
+CREATE DATABASE IF NOT EXISTS facultate;
+
+CREATE TABLE facultate.student 
+( idstudent int PRIMARY KEY AUTO_INCREMENT,
+nume VARCHAR(255) NOT NULL UNIQUE,
+prenume VARCHAR(255) NOT NULL UNIQUE,
+grupa INT NOT NULL CHECK ( grupa in (1,2,3,4)),
+email VARCHAR(255) UNIQUE,
+data_inscrierii DATETIME DEFAULT CURRENT_TIMESTAMP,
+statut VARCHAR(10) DEFAULT 'neevaluat' 
+	CHECK (statut in ('admis', 'respins', 'neevaluat'))
+); 
+
+USE facultate;
+SELECT * FROM student;
+
+INSERT INTO student SET nume = 'Georgescu', prenume = 'George', grupa = 1,
+email = 'george@facultate.ro';
+SELECT * FROM student;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Curs3/4.Facultate_studenti_relatie.sql
@@ -0,0 +1,31 @@
+USE mydb;
+
+SELECT * FROM student;
+SELECT * FROM facultate;
+
+INSERT INTO facultate SET NUME = 'Vietii', adresa = 'Str. Soarelui';
+SELECT * FROM facultate;
+
+SELECT * FROM student;
+INSERT INTO student SET nume = 'Andy', prenume = 'Ionut', grupa = 2, email = 'ionut@vietii.ro',
+	facultate_idfacultate = 1;
+SELECT * FROM student;
+
+INSERT INTO student SET nume = 'Anton', prenume = 'Pan', grupa = 2, email = 'anton@vietii.ro',
+	facultate_idfacultate = 1;
+SELECT * FROM student;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
